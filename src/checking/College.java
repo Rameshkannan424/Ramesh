e08098e7e61e323b8147ded76b500c0d33f9b573
Rameshkannan424 <rameshkannanmca18@gmail.com>
2021-02-06 20:46:59 +0530

    email added

--- src/checking/College.java
+++ src/checking/College.java
@@ -1,8 +1,48 @@
 package checking;
 
 public class College {
-	private void dep() {
+	
+	public void dep() {
+		System.out.println("MCA");
 
 	}
+	public void college() {
+	
+		System.out.println("ESEC");
+	}	
+	
+	public void unv() {
+	
+		System.out.println("Anna nv");
+	}
+	
+	public void Adress() {
+		
+		System.out.println("Adress");
+	}
 
+	public void Area() {
+		System.out.println("Area");
+		
+		}
+	private void phoneNum() {
+		System.out.println("9966885588");
+
+	}
+	
+	private void mailid() {
+
+		System.out.println("ramesh424@gmail.com");
+	}
+	
+	
+	public static void main(String[] args) {
+		College cl = new College();
+		cl.Adress();
+		cl.Area();
+		
+		
+	}
+	
+	
 }
