c18660524907ad05d37aec9a03bfb8b39c15916a
Rameshkannan424 <rameshkannanmca18@gmail.com>
2021-02-06 19:10:17 +0530

    Upadted

--- src/checking/College.java
+++ src/checking/College.java
@@ -1,8 +1,16 @@
 package checking;
 
 public class College {
+	
 	private void dep() {
+		System.out.println("MCA");
 
 	}
+	private void college() {
+	
+		System.out.println("ESEC");
+	}	
+	
+	
 
 }
